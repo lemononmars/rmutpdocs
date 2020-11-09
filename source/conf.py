3cac440f92934052ba5f26fa2dbf6013fe707872
Sakulbuth Ekvittayaniphon <sakulbuth@gmail.com>
2020-11-10 00:33:10 +0700

    new theme and thai font

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,8 @@ author = 'Sakulbuth Ekvittayaniphon'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+import sphinx_rtd_theme
+extensions = ['recommonmark','sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,9 +51,44 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_engine = 'xelatex'
+latex_elements = {
+    'fontpkg': r'''
+\setmainfont{TeX Gyre Termes}
+\setsansfont{TeX Gyre Heros}
+\setmonofont{TeX Gyre Cursor}
+    ''',
+    'preamble': r'''
+\XeTeXlinebreaklocale ”th”
+\XeTeXlinebreakskip = 0pt plus 0pt
+
+\usepackage{fontspec}
+\defaultfontfeatures{Mapping=tex-text}
+
+\newfontfamily{\thaifont}[Scale=MatchUppercase,Mapping=textext]{TH Sarabun New}
+\newenvironment{thailang}{\thaifont}{}
+\usepackage[Latin,Thai]{ucharclasses}
+
+\setTransitionTo{Thai}{\begin{thailang}}
+\setTransitionFrom{Thai}{\end{thailang}}
+
+\usepackage{setspace}
+\onehalfspacing
+
+\usepackage{polyglossia}
+\setdefaultlanguage{english}
+\setotherlanguage{thai}
+
+\AtBeginDocument\captionsthai
+    ''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
+latex_show_urls = 'footnote'
